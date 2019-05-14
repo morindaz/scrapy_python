fb71aa60b10021f9468de032e05cb0d606b4da71
morindaz <690596131@qq.com>
2019-05-14 10:43:08 +0800

    将数据存入mongodb

--- baidu_crawler/baidu/pipelines.py
+++ baidu_crawler/baidu/pipelines.py
@@ -5,15 +5,35 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import os
+import pymongo
+from pymongo import MongoClient
+# class BaiduPipeline(object):
+#     def process_item(self, item, spider):
+#         if not os.path.exists('./data1'):
+#             os.mkdir('./data1')
+#         name = item['name']
+#         text = item['text']
+#         name = name.replace('/', '\\')  # 将\进行转义
+#         with open('./data1/' + name + '.txt', 'a', encoding='utf-8') as f:
+#             f.write(text + '\n')
+#         return item
+
 
 
 class BaiduPipeline(object):
-    def process_item(self, item, spider):
-        if not os.path.exists('./data'):
-            os.mkdir('./data')
-        name = item['name']
-        text = item['text']
-        name = name.replace('/', '\\')  # 将\进行转义
-        with open('./data/' + name + '.txt', 'a', encoding='utf-8') as f:
-            f.write(text + '\n')
-        return item
+        def __init__(self, mongodbname='admin'):
+            client = MongoClient('127.0.0.1', 27017)
+            self.db = client[mongodbname]
+
+        def process_item(self, item):
+            postItem = dict(item)
+            self.db.scrapy.insert(postItem)
+
+            ## useless
+            # word=[{"_id":1,"name":"sssss"}]
+            # client = MongoClient('127.0.0.1',27017)
+            # mongodbName="admin"
+            # db = client[mongodbName]
+            #
+            # postItem = word
+            # db.scrapy.insert(postItem)
